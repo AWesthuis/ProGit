e18c132173024c30fefcd4635d3bf99fa339d6a1
AWesthuis <westhuis02@outlook.com>
2021-08-11 16:53:12 +0200

    First commit - from command line

---
+++ ProGit/ProGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
