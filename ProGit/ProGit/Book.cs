7b4fbfbfc7a2467a21e9adb60eaccd369f1da98d
AWesthuis <westhuis02@outlook.com>
2021-08-14 22:00:35 +0200

    Add class Book

---
+++ ProGit/ProGit/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGit
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
